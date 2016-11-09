f2d4719102876c130e871512897d4f15f88c835e
rado99 <rado__99@abv.bg>
2016-11-09 20:33:34 +0200

    updated again

--- connection.php
+++ connection.php
@@ -2,5 +2,5 @@
   //this is the file that contains database details and get if its connected and set charset
   //$connection
   $connection = mysqli_connect("127.0.0.1", "root", "", "roboclub");
-  mysqli_set_charset($link, utf8);
+  mysqli_set_charset($connection, utf8);
 ?>
