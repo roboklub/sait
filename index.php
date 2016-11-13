ff5286ffd69843b176b9f5377819d9c959fca6d3
pedroelloko <stillalivepd@abv.bg>
2016-11-13 14:16:59 +0200

    .

--- index.php
+++ index.php
@@ -8,6 +8,6 @@
   include 'templates/classic_header.php'; // include the header file becoause the DRY(Don't Repeat Yourself) convention and its easy to change things
   include 'templates/classic_navigation_bar.php'; // include the global navigation bar
   include 'templates/classic_footer.php'; // include the global footer
-
+  
 
 ?>
